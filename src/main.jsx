e8ff722cc45e00c4536a04073162346654edb7d8
bhupendra7754 <yadava9042@gmail.com>
2025-03-09 18:48:54 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css'; // Optional: For Tailwind or global styles
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+// import { Home } from "lucide-react";
+
+// export default function IconExample() {
+//   return <Home className="h-6 w-6 text-blue-500" />;
+// }
+
