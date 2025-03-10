c688d1a4b7c681a9854198fb659ad9ddd1b95960
bhupendra7754 <yadava9042@gmail.com>
2025-03-10 19:07:55 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind in React! 🚀
+        </h1>
+      </div>
+    );
+  }
+  
